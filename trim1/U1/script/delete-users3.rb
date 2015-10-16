4b5ebfe500719bb68c53b5e9c987331541388998
AntonioHD <antoniohda@gmail.com>
2015-10-16 10:34:20 +0100

    script haciendo uso de <if> y <count>

---
+++ trim1/U1/script/delete-users3.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/ruby
+#Encoding: utf-8
+
+#Antonio Hernandez Dominguez-Script eliminar usuarios desde .txt o .data
+
+system("clear")
+
+#Cargamos los usuarios de los ficheros en un array
+users=`cat userslist*`
+list=users.split("\n")
+
+#verificamos que se han cargado en el arrray y que los ficheros no estaban vacios
+if list.count==0
+   puts "no existen usuarios en el array"
+   puts "fin del script"
+   exit
+end
+
+#Eliminamos los usuarios especificados en el array
+list.each do |nombreusuario|
+  `sudo userdel #{nombreusuario}`
+  puts "se ha eliminado el usuario "+nombreusuario
+end
+
+puts "End of script"
