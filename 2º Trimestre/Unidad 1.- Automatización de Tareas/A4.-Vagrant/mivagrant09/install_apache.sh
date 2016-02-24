5f66ebeead612916c8a5830860f611fe2f749f5d
AntonioHD <antoniohda@gmail.com>
2016-02-24 09:01:25 +0000

    vagrant screenshots and files

---
+++ 2º Trimestre/Unidad 1.- Automatización de Tareas/A4.-Vagrant/mivagrant09/install_apache.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+    apt-get update
+    apt-get install -y apache2
+    rm -rf /var/www
+    ln -fs /vagrant /var/www
+    echo "<h1>Actividad de Vagrant</h1>" > /var/www/index.html
+    echo "<p>Curso201516</p>" >> /var/www/index.html
+    echo "<p>Antonio Hernandez Dominguez</p>" >> /var/www/index.html
